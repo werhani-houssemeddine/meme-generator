4ff1c3ded05c29abcd579302585dba488a649696
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2022-10-16 09:21:14 +0100

    I add some style and start using state to keep tracking the data written in the form

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,5 +1,12 @@
-import memeData from './data'
+import memeData from './data';
+import { useState } from 'react';
+
 function Form() {
+  const [meme, setMeme] = useState({
+    topText: '',
+    bottomText: '',
+    randomImage: 'http://i.imgflip.com/1bij.jpg',
+  });
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -14,15 +21,40 @@ function Form() {
     }
 
     getMemeImage();
+  };
+
+  function handleChange(event) {
+    const {name, value} = event.target;
+    
+    setMeme((prev) => ({ ...prev, [name]: value }));
   }
 
   return (
     <main>
       <form className="meme-form" onSubmit={submitHandler}>
-        <input type="text" placeholder="top text" className="form-input" />
-        <input type="text" placeholder="bottom text" className="form-input" />
+        <input
+          type="text"
+          name="topText"
+          placeholder="top text"
+          className="form-input"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="bottomText"
+          placeholder="bottom text"
+          className="form-input"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
         <button className="form-button">Get a new meme image 🖼</button>
       </form>
+      <div className="meme">
+        <img src={meme.randomImage} className="meme--image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   );
 }
